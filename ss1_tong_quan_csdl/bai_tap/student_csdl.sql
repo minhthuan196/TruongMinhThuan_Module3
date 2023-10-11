81e005ff2e67dcf348b1ce385ad0baa8a9e35c4a
minhthuan196 <minhthuandtu@gmail.com>
2023-10-11 15:37:21 +0700

    ss1

--- ss1_tong_quan_csdl/bai_tap/student_csdl.sql
+++ ss1_tong_quan_csdl/bai_tap/student_csdl.sql
@@ -1,40 +1,39 @@
-create database student;
-use student;
-create table student(
- id int,
- name varchar(200),
- age int,
- country varchar(50)
- );
- create table class(
- id int primary key,
- name varchar(200)
- );
- create  table teacher(
- id int primary key,
- name varchar(200),
- age int,
- country varchar(50)
- );
- alter table student
- add primary key (id);
- 
- insert into student(id,name,age,country)
- value (1,"Thuận",27,"Việt Nam");
-  insert into student(id,name,age,country)
- value (2,"Giang",22,"Việt Nam");
-  insert into student(id,name,age,country)
- value (3,"Hữu",24,"Việt Nam");
-   insert into student value (4,"Kiệt",22,"Việt Nam"),
-		(5,"Tuấn",18,"Việt Nam"),
-        (6,"Quyền",19,"Việt Nam");
-update student set country = "USA" where id = 1;
-update student set age = 10 where id =3;
-delete from student where id =1;
+create database student_management;
+use student_management;
+CREATE TABLE student (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_name VARCHAR(200),
+    student_age INT,
+    student_country VARCHAR(50)
+);
+CREATE TABLE class (
+    class_id INT PRIMARY KEY AUTO_INCREMENT,
+    class_name VARCHAR(200)
+);
+CREATE TABLE teacher (
+    teacher_id INT PRIMARY KEY AUTO_INCREMENT,
+    teacher_name VARCHAR(200),
+    teacher_age INT,
+    teacher_country VARCHAR(50)
+);
+ insert into student(student_name,student_age,student_country)
+ value ("Thuận",27,"Việt Nam"),
+		("Giang",22,"Việt Nam"),
+		("Hữu",24,"Việt Nam"),
+        ("Kiệt",22,"Việt Nam"),
+		("Tuấn",18,"Việt Nam"),
+        ("Quyền",19,"Việt Nam");
+update student set student_country = "USA" where student_id = 1;
+update student set student_age = 10 where student_id =3;
+delete from student where student_id =1;
 alter table student
-add gender varchar(10);
-update student set gender = "male" where id =2;
-alter table student modify column id int not null auto_increment;
-insert into student(name,age,country,gender) value ("Đạt",30,"Việt Nam","male");
+add student_gender varchar(10);
+UPDATE student 
+SET 
+    student_gender = 'male'
+WHERE
+    student_id = 2;
+insert into student(student_name,student_age,student_country,student_gender) 
+value ("Đạt",30,"Việt Nam","male");
 alter table student add test int;
 alter table student drop test;
