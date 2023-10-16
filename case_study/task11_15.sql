873b648ccc1cda9586ab08f94ee3eaf98aeb584b
minhthuan196 <minhthuandtu@gmail.com>
2023-10-16 22:16:18 +0700

    case study

--- case_study/task11_15.sql
+++ case_study/task11_15.sql
@@ -28,14 +28,36 @@ and ctr.id not in(
 group by ctr.id;
 -- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
 -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
-select *
-from (
-	select acs.id, ifnull(sum(dc.quantity),0) as total_accompanied_service
+with total as(
+	select acs.id,acs.name, ifnull(sum(dc.quantity),0) as total_accompanied_service
 	from detail_contract dc
 	join accompanied_service acs on acs.id = dc.accompanied_service_id
-	group by acs.id
-) as total_accompanied_service_table
-having total_accompanied_service = max(total_accompanied_service)
-;
+	group by acs.id)
+select *
+from total
+where total_accompanied_service = (select max(total_accompanied_service)
+from total);
+-- 14.Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung
+-- (được tính dựa trên việc count các ma_dich_vu_di_kem).
+select ctr.id as contract_id, ts.name as type_service_name, acs.name accompanied_service_name,
+count(dc.accompanied_service_id) as total_use_service
+from contract ctr
+join service sv on sv.id = ctr.service_id
+join type_service ts on ts.id = sv.type_service_id
+join detail_contract dc on dc.contract_id = ctr.id
+join accompanied_service acs on acs.id = dc.accompanied_service_id
+group by dc.accompanied_service_id
+having count(dc.accompanied_service_id) = 1
+-- 15. Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do,
+-- ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select s.id, s.name, l.name as levels, p.name as parts, s.phone, s.address
+from staffs s
+join parts p on p.id = s.parts_id
+join levels l on l.id = s.levels_id
+join contract c on c.staffs_id = s.id
+group by c.staffs_id
+having count(c.staffs_id) <3
+
 
 
